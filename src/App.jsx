f7540537d43aed764f7f62e7b35526319f80c555
Stephen Horvath <dev@stephenhorvath.com>
2022-01-06 16:43:55 +0100

    Lint

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ const App = () => {
   /*
   * Just a state variable we use to store our user's public wallet.
   */
+  // eslint-disable-next-line no-unused-vars
   const [_currentAccount, setCurrentAccount] = useState("");
 
   const checkIfWalletIsConnected = async () => {
