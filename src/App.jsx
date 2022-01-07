9665cc305c1262d78ba3d478209e675370259dd7
Stephen Horvath <dev@stephenhorvath.com>
2022-01-07 15:32:06 +0100

    Connect smart contract

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from "react";
+import { ethers } from "ethers";
 import "./App.css";
+import { abi as contractABI } from './utils/WavePortal.json';
+
+const contractAddress = '0xFD14cE75096bdb240aB5d1A8536A945CA4E0e7C0';
 
 const App = () => {
   /*
   * Just a state variable we use to store our user's public wallet.
   */
-  // eslint-disable-next-line no-unused-vars
   const [currentAccount, setCurrentAccount] = useState("");
+  const [totalWaves, setTotalWaves] = useState(-1);
 
   const checkIfWalletIsConnected = async () => {
     try {
@@ -36,10 +40,32 @@ const App = () => {
     }
   }
 
-   /**
+  /**
+   * Gets the current wave count.
+   */
+  const updateWaveCount = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        let count = await wavePortalContract.getTotalWaves();
+        setTotalWaves(count);
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  /**
     * Implement your connectWallet method here
     */
-   const connectWallet = async () => {
+  const connectWallet = async () => {
     try {
       const { ethereum } = window;
 
@@ -52,6 +78,34 @@ const App = () => {
 
       console.log("Connected", accounts[0]);
       setCurrentAccount(accounts[0]);
+      updateWaveCount();
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const wave = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        /*
+        * Execute the actual wave from your smart contract
+        */
+        const waveTxn = await wavePortalContract.wave();
+        console.log("Mining...", waveTxn.hash);
+
+        await waveTxn.wait();
+        console.log("Mined -- ", waveTxn.hash);
+
+        updateWaveCount();
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
     } catch (error) {
       console.log(error)
     }
@@ -75,7 +129,11 @@ const App = () => {
         I am Stephen. I love community and I'm a software developer and relationship coach. That's pretty awesome, right? Connect your Ethereum wallet and wave at me!
         </div>
 
-        <button className="waveButton" onClick={null}>
+        <div className="totalWaves">
+          {totalWaves < 0 ? 'Loading waves...' : `Check this out. I have ${totalWaves} waves!!!`}
+        </div>
+
+        <button className="waveButton" onClick={wave}>
           Wave at Me
         </button>
 
